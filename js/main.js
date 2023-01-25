89f9ec36365c2978dc8ce5477ab9c1616a1cb424
Pavel Komarov <pasha.revers@mail.ru>
2023-01-26 02:09:04 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    btnDarkMode.classList.add('dark-mode-btn-active');
+    btnDarkMode.classList.add('dark-mode-btn--active');
     document.body.classList.add('dark');
 }
 
 if (localStorage.getItem('darkmode') === 'dark') {
-    btnDarkMode.classList.add('dark-mode-btn-active');
+    btnDarkMode.classList.add('dark-mode-btn--active');
     document.body.classList.add('dark'); 
 }else if(localStorage.getItem('darkmode') === 'light'){
-    btnDarkMode.classList.remove('dark-mode-btn-active');
+    btnDarkMode.classList.remove('dark-mode-btn--active');
     document.body.classList.remove('dark');
 }
 
@@ -19,11 +19,11 @@ window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').addEventL
     const newColorSheme = event.matches ? 'dark' : 'light';
 
     if (newColorSheme === 'dark') {
-        btnDarkMode.classList.add('dark-mode-btn-active');
+        btnDarkMode.classList.add('dark-mode-btn--active');
         document.body.classList.add('dark');
         localStorage.setItem('darkmode', 'dark');
     }else{
-        btnDarkMode.classList.remove('dark-mode-btn-active');
+        btnDarkMode.classList.remove('dark-mode-btn--active');
         document.body.classList.remove('dark');
         localStorage.setItem('darkmode', 'light');
     }
@@ -31,8 +31,7 @@ window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').addEventL
 
 btnDarkMode.onclick = () => {
     btnDarkMode.classList.toggle('dark-mode-btn--active');
-    document.body.classList.toggle('dark');
-
+    
     const isDark = document.body.classList.toggle('dark');
 
     if (isDark) {
